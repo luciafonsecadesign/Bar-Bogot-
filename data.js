ad1b058b17c3af778b91282d729da6b87637bbaf
luciafonsecadesign <luciafonsecadesign@gmail.com>
2022-09-14 21:10:29 -0500

    Bar Bogota

---
+++ data.js
@@ -0,0 +1,163 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vista-general-1",
+      "name": "Vista general 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4014257279587117,
+        "pitch": 0.020171088709791007,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2769308776518624,
+          "pitch": 0.09794040882047561,
+          "rotation": 4.71238898038469,
+          "target": "1-vista-abierta-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-vista-abierta-1",
+      "name": "Vista abierta 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6148929210872431,
+        "pitch": 0.025671245291302824,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 4.71238898038469,
+          "target": "3-vista-general-cerrada-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-vista-cerrada-1",
+      "name": "Vista cerrada 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6135506990561268,
+        "pitch": 0.012601178195472684,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.40986867632483914,
+          "pitch": -0.059464537108269155,
+          "title": "Especificaciones",
+          "text": "*Mueble de bar exterior<div>*Estructura fabricada en aglomerado Rh color Espresso</div><div>*Entrepaños en vidrio</div><div>*Iluminación cálida en la parte interna</div><div>*Puertas batientes con marco metálico en pintura electrostática negra mate</div><div>*Vidrios ahumados&nbsp;</div><div>*Mesón en piedra sinterizada referencia: Snow River</div><div>*Repisas flotantes en madera natural con acabado en poliuretano color taupé con iluminación indirecta en la parte inferior</div><div>*Riel de iluminación con lámparas spot de color negro</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-vista-general-cerrada-1",
+      "name": "Vista general cerrada 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09532085900639764,
+        "pitch": -0.00550098113384756,
+        "fov": 1.4210867929104314
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06209628346870488,
+          "pitch": 0.002216298506043657,
+          "rotation": 4.71238898038469,
+          "target": "2-vista-cerrada-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BAR BOGOTÁ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
